3f04c6fafd8e7547d7139c8cf4b379fef0b37fe6
AcruxTech <acrux_tech@mail.ru>
2022-07-02 15:33:39 +0300

    initial setup, create home page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,61 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
+  <span class='author'><a href='https://github.com/AcruxTech'>powered by AcruxTech</a></span>
 </template>
 
 <style>
+@font-face {
+  font-family: 'OswaldMedium'; 
+  src: url('assets/fonts/Oswald-Medium.ttf') format('truetype'); 
+  font-style: normal; 
+  font-weight: normal; 
+} 
+@font-face {
+  font-family: 'OswaldRegular'; 
+  src: url('assets/fonts/Oswald-Regular.ttf') format('truetype'); 
+  font-style: normal; 
+  font-weight: normal; 
+} 
+@font-face {
+  font-family: 'OpenSansRegular'; 
+  src: url('assets/fonts/OpenSans-Regular.ttf') format('truetype'); 
+  font-style: normal; 
+  font-weight: normal; 
+} 
+@font-face {
+  font-family: 'SyneMedium'; 
+  src: url('assets/fonts/Syne-Medium.ttf') format('truetype'); 
+  font-style: normal; 
+  font-weight: normal; 
+} 
+@font-face {
+  font-family: 'SyneRegular'; 
+  src: url('assets/fonts/Syne-Regular.ttf') format('truetype'); 
+  font-style: normal; 
+  font-weight: normal; 
+} 
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background: rgb(255,171,44);
+  background: linear-gradient(166deg, rgba(255,171,44,1) 0%, rgba(25,25,25,1) 100%);
 }
 
-#nav {
-  padding: 30px;
+.author {
+  position: absolute;
+  bottom: 50px;
+  right: 50px;
+  font-family: 'OpenSansRegular';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 22px;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.author > a,
+.author > a:visited {
+  text-decoration: none;
+  color: #fff;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.author > a:hover {
+  text-decoration: underline;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class='home'>
-    <h1>This is a home page</h1>
+    <div class='center'>
+      <h1 class='title'>TrainFormuls</h1>
+      <h3 class='motto'>everything you need for a successful study of physics</h3>
+      <div class='buttons'>
+        <button type='button' class='button start'>Let`s start!</button>
+        <button type='button' class='button more'>Learn more</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,3 +16,48 @@ export default {
 
 }
 </script>
+
+<style scoped>
+.home {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.title {
+  font-family: 'OswaldMedium';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 48px;
+  line-height: 71px;
+  text-align: center;
+  letter-spacing: 0.02em;
+}
+.motto {
+  font-family: 'OswaldMedium';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 27px;
+  text-align: center;
+  text-transform: uppercase;
+}
+.buttons {
+  width: 30vw;
+  height: 10vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.button {
+  width: 40%;
+  height: 50%;
+  border: none;
+  border-radius: 15px;
+  outline: none;
+}
+.start {
+  background-color: #057D9F;
+}
+</style>
